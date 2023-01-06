8e93c3c56ee43c214c364aeb17ed2df3922f0ff2
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2023-01-06 23:55:04 +0530

    Create insta.cpp

---
+++ insta.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    if(x>10*y){
+	        cout<<"yes"<<endl;
+	        
+	    }
+	    else 
+	    {
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
